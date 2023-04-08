c2fede73a3282971e6e0d2dca151e103cdcd34f7
Hamza <hamzamunawar345@gmail.com>
2023-04-09 02:43:14 +0500

    Commiting all files in main branch

---
+++ Screens/ImageApiCall.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+//Tokens and URLS
+const access_key = "GEKLBNRZLr5utYXM6Qln6Rx7r9vc3MOqhSH0OVmBxLEPrBWsWwY5UYpL";
+const url = "https://api.pexels.com/v1/photos/2014422";
+
+export default function ImageApiCall() {
+  const [photosData, setPhotosData] = useState("");
+  useEffect(() => {
+    getPhotos();
+  }, []);
+  const getPhotos = async () => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: access_key,
+      },
+    });
+    const data = await response.json();
+    setPhotosData(data);
+  };
+  console.log(photosData.alt);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textContainer}> {photosData.height}</Text>
+      <Image source={{ uri: photosData.src.small }} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
